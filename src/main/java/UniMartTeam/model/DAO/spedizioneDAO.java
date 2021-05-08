a8e1a3ef761e6c44f399bbbdf27f92ab6d9f81b4
Sabato <sabatinogenovese51@gmail.com>
2021-05-08 18:58:39 +0200

    Creato spedizioneDAO.java

--- src/main/java/UniMartTeam/model/DAO/spedizioneDAO.java
+++ src/main/java/UniMartTeam/model/DAO/spedizioneDAO.java
@@ -2,5 +2,21 @@ package UniMartTeam.model.DAO;
 
 public class spedizioneDAO
 {
-    we
+   /*doSave(DATA o)
+   salva i dati nel database(insert)
+
+   doSaveOrUpdate(DATA o)
+   salva o aggiorna l'oggetto in questione
+
+   doDelete(Key k)
+   elimina l'oggetto con key = k
+   posso usare anche doDelete(DATA o)
+
+   doRetiveAll(limits)
+   restituisce tutti gli oggetti entro i limiti
+
+doRetriveByCond(Condizione)
+   restituisce una collezione di oggetti data una condizione
+
+   doRetriveByKey(Key k)*/
 }
